6a538a300bfdd19f72768a4b7b703a5b5358a5d2
Gabriel Aquiles Silva Araújo <gabriel61436616@edu.df.senac.br>
2026-03-19 14:44:52 -0300

    Adicionei um script em python de verificação de site

---
+++ Verificação.py
@@ -0,0 +1,8 @@
+m = 18
+result = int(input("Quantos anos você tem? "))
+
+if result >= m:
+    print ("Bem vindo ao site! Aproveite com maturidade")
+
+else:
+    print ("Você não tem idade suficiente para entrar nesse site. Tente com outra conta.")
